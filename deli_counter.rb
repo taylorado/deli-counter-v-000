8da9d292d6a72ca817e697e5c899f4caf472d9f5
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 20:21:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli = []
 
 
 def line(katz_deli)
-  if katz_deli.count == 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
 
 
@@ -13,7 +13,6 @@ def line(katz_deli)
     listOutput = "The line is currently:"
       katz_deli.each_with_index do |item, index|
        listOutput += " #{index+1}. #{item}"
-  #    index +=1
     end
     puts listOutput
   end
@@ -22,12 +21,12 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 
 end
 
 def now_serving(katz_deli)
-    if katz_deli.count == 0
+    if katz_deli.length == 0
       puts "There is nobody waiting to be served!"
     else
       puts "Currently serving #{katz_deli[0]}."
