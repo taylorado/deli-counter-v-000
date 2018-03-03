a8a20a5ca0981df623e93956ce15e55d441a5245
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 20:15:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,6 +25,7 @@ def take_a_number(katz_deli, name)
     puts "The line is currently empty."
 
   else
+    katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
   end
 end
