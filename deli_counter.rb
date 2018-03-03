cb21a7bc407b2e7ae59d60334e7e8a7cd95599ed
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 19:58:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
-    katz_deli.each { |i| puts "#{i}. #{katz_deli[i]}"}
+    puts "The line is currently: #{katz_deli}"
+#    katz_deli.each { |i| puts "#{i}. #{katz_deli[i]}"}
   #    index +=1
 #    end
   end
