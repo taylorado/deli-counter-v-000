be1d2a29f255798e9ed6516aec621c6ded76bca2
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 20:18:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,11 +21,16 @@ end
 
 
 def take_a_number(katz_deli, name)
-#  if katz_deli.count == 0
-#    puts "The line is currently empty."
-#  end
-
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 
 end
+
+def now_serving(katz_deli)
+    if katz_deli.count == 0
+      puts "The line is currently empty."
+    else
+      puts "Currently Serving #{katz_deli[0]}"
+      katz_deli.shift
+    end
+  end
